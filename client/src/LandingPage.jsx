4de75794267ad2c7a373f6a3d72e9f9ed9dcea83
rohitmehta18 <rmehta1836@gmail.com>
2025-03-17 12:45:15 +0530

    done

---
+++ client/src/LandingPage.jsx
@@ -0,0 +1,11 @@
+function LandingPage() {
+    return (
+      <div style={{ textAlign: "center", padding: "20px" }}>
+        <h1>Welcome to the ASAP Project</h1>
+        <p>Discover Makeout Places in Our Campus 🚀</p>
+      </div>
+    );
+  }
+  
+  export default LandingPage;
+  
