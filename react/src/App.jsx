ab272efa998298f30c0627baeadc0177c322447b
rohitmehta18 <rmehta1836@gmail.com>
2025-04-18 20:38:05 +0530

    Adding relations between entities

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,15 @@
-// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import EditSpot from "./pages/EditSpot";
+import AddSpot from "./pages/AddSpot";
+import EditSpot from "./pages/EditSpot"; // assuming you already have this
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddSpot />} />
         <Route path="/edit/:id" element={<EditSpot />} />
       </Routes>
     </Router>
